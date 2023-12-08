524507c77b3aebee643982ba5dde43a0428e9d32
Sinx <gabriel.jbrito2@gmail.com>
2023-12-08 10:50:07 -0300

    criando e usando temas

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -19,7 +19,7 @@ class Header extends StatelessWidget {
         ),
         borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
         ),
-      child: const Padding(
+      child:  Padding(
         //Padding from LTRB significa left, top, right e bottom nessa ordem
         padding: EdgeInsets.fromLTRB(16, 80, 16, 16),
         child: Row(
@@ -39,21 +39,16 @@ class Header extends StatelessWidget {
                 Text.rich(
                   TextSpan(
                     text: '\$',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
                     children: [
                       TextSpan(
                         text: '1000.00',
-                        style: TextStyle(
-                            fontSize: 28, fontWeight: FontWeight.bold),
+                        style: Theme.of(context).textTheme.bodyLarge,
                       ),
                     ],
                   ),
                 ),
                 Text(
                   'Balanço disponível',
-                  style: TextStyle(fontSize: 16),
                 )
               ],
             ),
---
+++ lib/themes/my_theme.dart
@@ -0,0 +1,17 @@
+import 'package:alubank/themes/themes_color.dart';
+import 'package:flutter/material.dart';
+
+ThemeData MyTheme = ThemeData(
+  primarySwatch: ThemeColors.primaryColor,
+  primaryColor: ThemeColors.primaryColor,
+  brightness: Brightness.dark, // define o tema para dark mode
+  textTheme: TextTheme(
+    bodyMedium: TextStyle(
+      fontSize: 16,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+);
